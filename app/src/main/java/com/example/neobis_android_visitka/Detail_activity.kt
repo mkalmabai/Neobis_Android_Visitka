ae83a6a1a4cbd1bce1a36b52897871d3d9eca339
Margulan <akudajkul7@gmail.com>
2023-10-24 00:50:02 +0600

    finished all design!

---
+++ app/src/main/java/com/example/neobis_android_visitka/Detail_activity.kt
@@ -0,0 +1,11 @@
+package com.example.neobis_android_visitka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detail_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/example/neobis_android_visitka/MainActivity.kt
+++ app/src/main/java/com/example/neobis_android_visitka/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.neobis_android_visitka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val doctor1Layout = findViewById<LinearLayout>(R.id.doctor1)
+
+        doctor1Layout.setOnClickListener {
+            val intent = Intent(this, Detail_activity::class.java)
+            startActivity(intent)
+        }
     }
 }
